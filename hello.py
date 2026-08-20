d4890f51ddd62ecf65fa94be039023f23396d655
MuneebMushtaq <moonmoon285426@gmail.com>
2026-08-20 20:56:06 +0500

    Add hello.py with basic print statements and variables

---
+++ hello.py
@@ -0,0 +1,23 @@
+print("hello world")
+print(" i am learning the github and uploading my learning into the git hub account")
+#  this is the process in which i learning the how to print the different things
+age=20.44444444444444444444
+number=-34
+grade=00000000000000000000.4565
+name="Muneeb"
+grade='A'
+phone_number=3022854343342234423
+print(age)
+print(number)
+print(grade)
+print(name)
+print(phone_number)
+#  how to print the types of the data which data is this is it int or string or float
+print(type(age))
+print(type(number))
+print(type(grade))
+print(type(phone_number))
+print("the type of name is:", type(name))
+num=2
+isPrime=True
+print(isPrime)
