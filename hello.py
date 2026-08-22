e855c4f61c2c8772b4d3edfec2b79bcb7f2c8394
MuneebMushtaq <moonmoon285426@gmail.com>
2026-08-22 20:46:18 +0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -21,3 +21,196 @@ print("the type of name is:", type(name))
 num=2
 isPrime=True
 print(isPrime)
+
+
+print("my name is muneeb\ni am a student of B.S Artificial Intelligence:")
+'''
+above is multiline comment 
+and this is also a multiline comment'''
+''' this is also a multiline comment
+and this is also a multiline comment'''
+
+
+LETS START THE NEEW JOURNEY OF PYTHON PROGRAMMING FROM BELOW AND NOW ONWARDS 
+
+print("""start the coding as soon  as possible and dont waste your time in thinking about the code 
+      and just start writing the code 
+      and you will see the magic of coding
+      and you will love it more """)
+
+
+                    THE new lesson of the arthmetic operator is started
+
+a=10
+b=3
+
+print("the value of a is:",a)
+print("the value of b is:",b)
+print("the sum of a and b is:", sum)
+print("the sub of a and b is :",a-b) 
+print("the product of a and b is :",a*b) 
+print("the division of a and b is :",a/b) 
+
+print("the sum of a +b is:",a+b)
+print("the module of a and b is:",a%b) 
+what is the  mean of the module operator in python?
+
+The module operator (%) returns the remainder of the division of the first operand by the second operand.
+print("the moodule of 25 and 7 is:",25%7)
+  
+
+to find the power of any number we can find power  simply by using a**b where a is the base and b is the power.
+print("the power of 5 to the 4  is:",5**4)
+
+
+
+
+   what is the relational and comparison operator in python?
+
+  Relational and comparison operators in Python are used to compare values and return a boolean result (True or False). The common relational and comparison operators include:
+
+a =5
+b=3
+ a is equal to b
+print(a==b)
+  a is not equal to b
+print(a!=b)
+print(5==5)
+ a is greater than the b
+print(a>b)
+print(10<15)
+print(2>3)
+print(5!=10)
+print(4>=5)
+print(5<=5)
+
+                 what is the assignment operator in python?
+Assignment operators in Python are used to assign values to variables. The common assignment operators include
+
+
+
+  a=10
+b=5
+a=b
+print(a)
+print(b)
+ adding one into the a
+a+=1
+print (a)
+a-=2
+ subtracting 2 from the a
+print(a)
+a*=2
+print(a)
+a-=8
+print(a)
+
+
+a=2
+b=10
+a=b
+a+=5
+print(a)
+a-=10
+print(a)
+a/=5
+print(b)
+print(a)
+
+printing the modules
+a%=a 
+print(a)
+
+
+
+                    now we are using logical opeators in python
+                  the use of the OR operator in python
+
+
+                 
+var=False
+print(not(var))
+print(not(5<10))
+print(not(10>15))
+print(not(2==2))
+print(not("python"=="C++"))
+                                   operator precedence in python
+print(10*(10*2)*2**2+4-5)
+chosing which operator to used
+print(2+2+(2-5)+2**2/4)
+
+              TYPE CONVERSION IN PYTHON
+            IMPLICIT TYPE CONVERSION IN PYTHON
+
+
+a=5
+b=10
+print(a+b)
+
+printing the type is it the int or float or other 
+print(type(a+b))
+a=5
+b=10.55555
+print(a+b)
+print(type(a+b))
+
+
+Explicit type conversion in python and Casting in python
+
+
+
+a=5
+b=10.5
+ans=a+b
+converting ans into  the int value
+print(int(ans),type(ans))
+print(ans, type(ans))
+print(int(10+12.4))
+print(type(10+12.5))
+
+ a=input("enter the number:")
+ a=input("enter the number:")
+print(a)
+name=input("enter your nmae:")
+print("welcome",name)   
+name=input("enter your nmae:")
+print("your name is:",name)
+
+
+taking the input
+
+a=int(input("enter the first number:"))
+b=float(input("enter the second number:"))
+print("the sum of a and b is:",a+b)
+ i want to take input of my name id uni etc
+name=input("enter the name:")
+id=input("enter the id:")
+uni=input("enter the uni:")
+print("the name of the student is:",name)
+print("uni name:",uni)
+print("enter the id:",id)
+
+
+taking the sum subtract and multiplication and division of the data
+
+
+
+a=int(input("enter the number:"))
+b=int(input("enter the number:"))
+sum=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("the sum is:",sum)
+print("the sub is:",sub)
+print("the mul is:",mul)
+print("the div is:",div)
+a =int(input("enter the value of a:"))
+b =int(input("enter the value of b:"))
+sum=a+b
+print("the sum of the numbers is:",sum)
+avg=sum/2
+print("the average of a and b is:",avg)
+
+
+
