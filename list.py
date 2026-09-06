89e506f7b7a957bd8aace292bc7593e68692a485
MuneebMushtaq <moonmoon285426@gmail.com>
2026-09-06 20:16:07 +0500

    Create list.py with basic list operations
    Added a Python script demonstrating list operations including printing, modifying, and slicing lists.

---
+++ list.py
@@ -0,0 +1,37 @@
+LETS NOW LEARN ABOUT THE LIST OF THE PYTHON
+
+marks=[99,9,98,56,45]
+PRINT THE MARKS OF ALL THE VALUES
+print(marks)
+print(len(marks))
+
+PRINT THE LENGTH OF THE MARKS 
+
+print(marks[2])
+
+THIS PRINT THE MARKST THE INDEX 2
+
+marks[2]=99
+
+this cahnge the value of the index  to new value
+print(marks)
+
+print(type(marks))
+this print the type of the marks
+
+
+
+WE CAN ALSO DO SLICING IN THE LIST 
+SIMPLY BY PASSING SLICE START AND THE END POINT
+
+print(marks[2:4])
+
+num=[1,2,3]
+
+
+num.append(4)
+append is used to add the value at the end of the index
+
+print(num)
+
+
