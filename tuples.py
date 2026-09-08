2c99d852a11933a058ba6e05fefcc21c9337d0a4
MuneebMushtaq <moonmoon285426@gmail.com>
2026-09-08 15:16:26 +0500

    Add examples for tuple operations in tuples.py
    Added examples demonstrating tuple usage, slicing, and looping.

--- tuples.py
+++ tuples.py
@@ -1,1 +1,31 @@
+TUPLE ARE LIKE THE LIST WE USE [] IN THE LIST AND () THIS ONE IN THE LIST
+age=(1,2,3,4,5,6)
+print(age)
+
+SIMILARLY WE CAN ALSO DO SLICING IN IT
+print(age[:3]
+
+
+       THE IUSE OF THE LOOP ON THE TUPLES
+  age=(1,2,3,4,5,6)
+for i in age:
+      print(i)
+
+
+
+THE USE OF TEH LOOP AND ALSO THE F STREAM STRING 
+age=(1,2,3,4,5)
+sum=0
+for i in age:
+      sum+=i
+print(f"sum of the value is the is {sum}")
+
+
+WE CAN ALSO PRINT IT BY USE OF TEH INDEX
+
+print(age[0])
+
+
+
+
 
